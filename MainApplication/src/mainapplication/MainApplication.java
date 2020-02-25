a7fbb08c13eabcf1c7b5edfa7dae982b204d08f7
Ariel Volovik <cstuser@10.1.3.228>
2020-02-25 08:55:55 -0500

    Fixing previous commit.

--- MainApplication/src/mainapplication/MainApplication.java
+++ MainApplication/src/mainapplication/MainApplication.java
@@ -1,20 +1,9 @@
 package mainapplication;
 
-import java.io.IOException;
 import javafx.application.Application;
-import javafx.event.EventHandler;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.TextField;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 /**
